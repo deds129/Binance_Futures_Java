7e9ecc6a84cca90d5ddc00f486819bf117aae10f
deds129 <nik.chudinov129@mail.ru>
2022-12-06 13:18:14 +0300

    Move UserDataCache to Redis ( use as db)

--- src/main/java/com/binance/client/model/enums/TimeInForce.java
+++ src/main/java/com/binance/client/model/enums/TimeInForce.java
@@ -1,5 +1,11 @@
 package com.binance.client.model.enums;
-
+/*
+	* GTC (Good-Till-Cancel): the order will last until it is completed or you cancel it.
+	
+	* IOC (Immediate-Or-Cancel): the order will attempt to execute all or part of it immediately at the price and quantity available, then cancel any remaining, unfilled part of the order. If no quantity is available at the chosen price when you place the order, it will be canceled immediately. Please note that Iceberg orders are not supported.
+	
+	* * FOK (Fill-Or-Kill): the order is instructed to execute in full immediately (filled), otherwise it will be canceled (killed). Please note that Iceberg orders are not supported.
+ */
 public enum  TimeInForce {
     GTC,
     IOC,
